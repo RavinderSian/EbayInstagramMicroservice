c5de582fe6105f066166639d87767351ccbcf152
ravinder sian <rsian761@gmail.com>
2023-05-29 18:55:19 +0100

    add initial persistence project

---
+++ ebayinstagram/ebayinstagram-persistence/src/main/java/com/personal/ebayinstagram/EbayinstagramPersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.personal.ebayinstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbayinstagramPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbayinstagramPersistenceApplication.class, args);
+	}
+
+}
