9105034e8d9d0d6f1ae4624abf67a6f1de170c06
ravinder sian <rsian761@gmail.com>
2023-06-03 22:02:18 +0100

    add ebay rest project

---
+++ ebayinstagram/ebayinstagram-ebay-api/src/main/java/com/personal/ebayinstagram/EbayinstagramEbayRESTApplication.java
@@ -0,0 +1,13 @@
+package com.personal.ebayinstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbayinstagramEbayRESTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbayinstagramEbayRESTApplication.class, args);
+	}
+
+}
