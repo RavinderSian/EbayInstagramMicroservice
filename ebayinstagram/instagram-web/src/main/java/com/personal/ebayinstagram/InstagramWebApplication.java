bc60478cda4199a338ef17800b53c16954c515b4
ravinder sian <rsian761@gmail.com>
2023-06-17 14:56:54 +0100

    initial changes, add thymeleaf dependency and controller

--- ebayinstagram/instagram-web/src/main/java/com/personal/ebayinstagram/InstagramWebApplication.java
+++ ebayinstagram/instagram-web/src/main/java/com/personal/ebayinstagram/InstagramWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InstagramRESTApplication {
+public class InstagramWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InstagramRESTApplication.class, args);
+		SpringApplication.run(InstagramWebApplication.class, args);
 	}
 
 }
